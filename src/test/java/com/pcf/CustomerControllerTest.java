2fb42d13fefda1e951c3daa1e4981330d60f0bd3
sumit pareek <sumit.pareek@mastercard.com>
2019-08-23 11:48:04 +0530

    initial test commit

---
+++ src/test/java/com/pcf/CustomerControllerTest.java
@@ -0,0 +1,13 @@
+package com.pcf;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class CustomerControllerTest {
+
+	@Test
+	public void testController() {
+		assertTrue(1==1);
+	}
+}
